14ef3c214c48f9f7bb2b6165ba93bcf5c7ed8b60
Ozbaab <ozbaab@hotmail.com>
2025-03-18 17:45:37 +0100

    finised

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from stats import num_of_words, count_characters, print_pretty
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         content = f.read()
     return content
 
-def main():
+def main(path):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}")
 
-    text = (get_book_text("books/frankenstein.txt"))
+    text = (get_book_text(path))
     print("----------- Word Count ----------")
     print(f'Found {num_of_words(text)} total words')
     
@@ -21,4 +22,8 @@ def main():
             continue
         print(f"{item['Character']}: {item['Count']}")
 
-main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+path = sys.argv[1]
+main(path)
